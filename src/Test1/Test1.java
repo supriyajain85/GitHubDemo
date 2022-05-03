4d23c5c14c38c051e2a83fe9bd5212547ba3e6a0
vishajai <vishajai@LNAR-5CG842668W.corp.capgemini.com>
2022-05-03 09:51:07 -0500

    First Commit

---
+++ src/Test1/Test1.java
@@ -0,0 +1,16 @@
+package Test1;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+
+		int a=5,  b=10;
+		int sum=0;
+		
+		 sum=a+b;
+		System.out.println("sum is " + sum);
+		
+		
+	}
+
+}
